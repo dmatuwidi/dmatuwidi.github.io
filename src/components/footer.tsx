75ea90635947313cc6fde84e728fa7737697d76f
Damy Matuwidi <dmatuwidi@gmail.com>
2025-03-30 14:52:29 +0100

    Added photo credits

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,10 @@ export default function Footer() {
   return (
     <footer className="flex flex-col border-t p-5 px-20">
       <Separator className="my-5" />
-      <div className="flex justify-center">
+      <div className="flex justify-center space-x-10">
         <p className="text-muted-foreground">&copy; 2025 Damy Matuwidi</p>
+        <p className="text-muted-foreground">Photo by <a href="https://unsplash.com/@lukethornton?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Luke Thornton</a> on <a href="https://unsplash.com/photos/mountains-are-shrouded-in-thick-blue-clouds-nHQFxJT4qOo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Unsplash</a>
+        </p>
       </div>
     </footer>
   )
