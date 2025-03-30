1cce5382ab1705a14de5deaf25f017aab630634d
Damy Matuwidi <dmatuwidi@gmail.com>
2025-03-30 17:57:16 +0100

    Cleaned up footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,13 +2,10 @@ import { Separator } from "./ui/separator"
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col border-t p-5 px-20">
-      <Separator className="my-5" />
-      <div className="flex justify-center space-x-10">
-        <p className="text-muted-foreground">&copy; 2025 Damy Matuwidi</p>
-        <p className="text-muted-foreground">Photo by <a href="https://unsplash.com/@lukethornton?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Luke Thornton</a> on <a href="https://unsplash.com/photos/mountains-are-shrouded-in-thick-blue-clouds-nHQFxJT4qOo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Unsplash</a>
-        </p>
-      </div>
+    <footer className="flex justify-center space-x-10 border-t p-5 px-20">
+      <p className="text-muted-foreground">&copy; 2025 Damy Matuwidi</p>
+      <p className="text-muted-foreground">Photo by <a href="https://unsplash.com/@lukethornton?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Luke Thornton</a> on <a href="https://unsplash.com/photos/mountains-are-shrouded-in-thick-blue-clouds-nHQFxJT4qOo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" className="underline">Unsplash</a>
+      </p>
     </footer>
   )
 }
